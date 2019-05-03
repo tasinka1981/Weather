ad40bf8fa10042d025d072300845761dc35d7ae8
tasinka1981 <tasinka1981@gmail.com>
2019-05-03 14:14:18 +0300

    Updates

---
+++ precache-manifest.5207c22eaf2a0bb2164c4f4bdb7129d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "31619654396534303aca3118c3686071",
+    "url": "/Weather/static/media/wind.31619654.svg"
+  },
+  {
+    "revision": "4cb5efd64cd06d4a0ac5f377c3b257f9",
+    "url": "/Weather/static/media/sun.4cb5efd6.svg"
+  },
+  {
+    "revision": "b6ab548097aebd7531d0190879079827",
+    "url": "/Weather/static/media/gps.b6ab5480.svg"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "27940640f3c000413857",
+    "url": "/Weather/static/js/main.3d2a20ee.chunk.js"
+  },
+  {
+    "revision": "5dd81de4cdbb21b52386",
+    "url": "/Weather/static/js/2.f82aa098.chunk.js"
+  },
+  {
+    "revision": "27940640f3c000413857",
+    "url": "/Weather/static/css/main.27b61cb4.chunk.css"
+  },
+  {
+    "revision": "5dd81de4cdbb21b52386",
+    "url": "/Weather/static/css/2.1623c128.chunk.css"
+  },
+  {
+    "revision": "5048c0548c416d002052790ec843701e",
+    "url": "/Weather/index.html"
+  }
+];
